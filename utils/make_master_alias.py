bbda055529dcb650db8f4a7ef8d95018193de82a
Rachel Plesha <rplesha@stsci.edu>
2021-03-01 16:37:00 -0500

    Updates to include the low metallicity star name parsing. This was added in for the JIRA tickets a while ago, but wasn't included in the website changes.
    - Added parameter "lowz" to make_master_aliases and parse_csv.
    - Skipped the row with WFC3 included in parse_csv. I did this only for the metallicity spreadssheets because it doens't work with the LMC/SMC because of archival data and it doesn't work with the ttauri stars because there is no ULL column in the csv files. These are needed in the csv file because of the JIRA tickets they track, otherwise they could be deleted.
    - Minor update to the change_names function to print a more useful message

--- utils/make_master_alias.py
+++ utils/make_master_alias.py
@@ -16,25 +16,26 @@ FUSEFILE2 = "inputs/fuse_aliases2.json"
 
 def parse_inputs():
     # First, define all input files and parse them correctly
-    aliases = pd.read_csv(JULIAFILE, comment="#") 
+    aliases = pd.read_csv(JULIAFILE, comment="#")
     # Remove leading or trailing spaces from both data and column names
-    aliases = aliases.applymap(lambda x: x.strip() if isinstance(x, str) else x)  
-    aliases.columns = aliases.columns.str.strip() 
+    aliases = aliases.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    aliases.columns = aliases.columns.str.strip()
     # All data should have a simbad_name entry, even if it is nan
     aliases["simbad_name"] = np.nan
-    # Add missing TTS if not present 
+    # Add missing TTS if not present
     mask = aliases.apply(lambda row: row.astype(str).str.fullmatch(re.escape("TX-ORI")).any(), axis=1)
     if set(mask) == {False}:
         aliasescols = aliases.columns.values
         aliases.loc[len(aliases)] = ["TX-ORI", "V-TX-ORI"] + [np.nan for i in range(len(aliasescols)-2)]
-    
+
     # Now read the preferred names files from the tech repo
     smc = parse_name_csv("smc", returndf=True)
     lmc = parse_name_csv("lmc", returndf=True)
     # The LMC file has a weird column that should be deleted
     lmc.drop(["Unnamed: 6"], axis=1, inplace=True)
     tts = parse_name_csv("tts", returndf=True)
-    
+    lowz = parse_name_csv("lowz", returndf=True)
+
     # Rename the columns to match columns in Julia's alias file. One map for Mag. clouds, one for TTS
     mcmap = {"Alternate_APT": "ULL_MAST_name",
              "PREFERRED": "ULL_name",
@@ -42,15 +43,16 @@ def parse_inputs():
              "APT": "alias1",
              "SIMBAD_SEARCH_NAME": "simbad_name",
              "ORIGINAL": "alias2"}
-    ttmap = {"Alternate_APT": "ULL_MAST_name", "PREFERRED": "ULL_name"} 
+    ttmap = {"Alternate_APT": "ULL_MAST_name", "PREFERRED": "ULL_name"}
     lmc.rename(columns=mcmap, inplace=True)
-    smc.rename(columns=mcmap, inplace=True) 
-    tts.rename(columns=ttmap, inplace=True) 
+    smc.rename(columns=mcmap, inplace=True)
+    tts.rename(columns=ttmap, inplace=True)
+    lowz.rename(columns=ttmap, inplace=True)
 #    # What is this for...?
 #    tts["ULL_name"] = tts["ULL_MAST_name"]
-    for df in [aliases, lmc, smc, tts]:
+    for df in [aliases, lmc, smc, tts, lowz]:
         df = df.apply(lambda x: x.astype(str).str.upper())
-    return  aliases, lmc, smc, tts
+    return  aliases, lmc, smc, tts, lowz
 
 
 
@@ -67,15 +69,15 @@ def add_aliases(comparison, all_aliases, verbose=False):
     to all_aliases.
     """
     for i in range(len(comparison)):
-        already_present = False 
-        for col in comparison.columns: 
+        already_present = False
+        for col in comparison.columns:
             targ = comparison.iloc[i][col]
             # See if there is any overlap
-            mask = all_aliases.apply(lambda row: row.astype(str).str.fullmatch(re.escape(targ)).any(), axis=1) 
+            mask = all_aliases.apply(lambda row: row.astype(str).str.fullmatch(re.escape(targ)).any(), axis=1)
             # If there is overlap, see if there are any comparison aliases to append
-            if set(mask) != {False}: 
-                existing = all_aliases[mask].values[0] 
-                rowupdate = comparison.iloc[i] 
+            if set(mask) != {False}:
+                existing = all_aliases[mask].values[0]
+                rowupdate = comparison.iloc[i]
                 toadd = list(set(rowupdate) - set(existing))
                 if len(toadd) == 0: # there are no comparison aliases
                     already_present = True
@@ -87,14 +89,14 @@ def add_aliases(comparison, all_aliases, verbose=False):
                 # are not, add new columns with appropriate names
                 emptycols0 = all_aliases.columns[all_aliases[mask].isna().any()].tolist() # could include simbad_targname
                 emptycols = [x for x in emptycols0 if x != "simbad_targname"]
-                if len(toadd) > len(emptycols): 
-                    for comparisoncol in range(len(toadd)-len(emptycols)): 
-                        aliasno = len(all_aliases.columns)-2 
-                        all_aliases[f"alias{aliasno}"] = np.nan 
-                    emptycols = all_aliases.columns[all_aliases[mask].isna().any()].tolist() 
-                
-                for j in range(len(toadd)): 
-                    all_aliases.loc[mask,emptycols[j]] = toadd[j] 
+                if len(toadd) > len(emptycols):
+                    for comparisoncol in range(len(toadd)-len(emptycols)):
+                        aliasno = len(all_aliases.columns)-2
+                        all_aliases[f"alias{aliasno}"] = np.nan
+                    emptycols = all_aliases.columns[all_aliases[mask].isna().any()].tolist()
+
+                for j in range(len(toadd)):
+                    all_aliases.loc[mask,emptycols[j]] = toadd[j]
                 already_present = True
                 # Add the simbad name if it exists for a given target and is
                 # a valid value
@@ -140,7 +142,7 @@ def fix_pipes(all_aliases):
                 emptycols = [x for x in emptycols0 if x != "simbad_targname"]
             for j in range(len(targs)):
                 all_aliases.loc[i, emptycols[j]] = targs[j]
-            
+
     return all_aliases
 
 # NOT IN USE
@@ -157,7 +159,7 @@ def create_fuse_alias2():
     for i in range(len(fuse_targs0)):
         fuse_d = {}
         fuse_d["dirname"] = fuse_targs_upper[i]
-        
+
         partial_d = fuse_targs0[i]
         partial_d = partial_d.replace("[", "LEFTBRACKET")
         partial_d = partial_d.replace("]", "RIGHTBRACKET")
@@ -173,11 +175,11 @@ def create_fuse_alias2():
     df = pd.DataFrame(fuse_dicts)
     df = df.apply(lambda x: x.astype(str).str.upper())
     df.to_json("inputs/fuse_target_aliases.json", orient="split")
-    print("Wrote inputs/fuse_target_aliases.json") 
+    print("Wrote inputs/fuse_target_aliases.json")
 
 
 # manual_insert.json includes manually matched FUSE names to ULLYSES names.
-# This only needs to be run once and never again. 
+# This only needs to be run once and never again.
 def create_fuse_alias(infile="inputs/manual_insert.json"):
     fuse_targs0 = glob.glob("/astro/ullyses/ullyses_target/data/SMC/FUSE/*") + \
                   glob.glob("/astro/ullyses/ullyses_target/data/LMC/FUSE/*") + \
@@ -205,39 +207,39 @@ def create_fuse_alias(infile="inputs/manual_insert.json"):
 
         toadd = list(set(toadd))
 
-        emptycols = df.columns[df.iloc[i].isna()].tolist() 
-        if len(toadd) > len(emptycols): 
-            for comparisoncol in range(len(toadd)-len(emptycols)): 
-                aliasno = len(df.columns)-1 
-                df[f"alias{aliasno}"] = np.nan 
+        emptycols = df.columns[df.iloc[i].isna()].tolist()
+        if len(toadd) > len(emptycols):
+            for comparisoncol in range(len(toadd)-len(emptycols)):
+                aliasno = len(df.columns)-1
+                df[f"alias{aliasno}"] = np.nan
             emptycols = df.columns[df.iloc[i].isna()].tolist()
-        for j in range(len(toadd)): 
+        for j in range(len(toadd)):
             df.loc[i,emptycols[j]] = toadd[j]
 
 
 
     df = df.apply(lambda x: x.astype(str).str.upper())
     df.to_json(FUSEFILE1, orient="split")
-    print(f"Wrote {FUSEFILE1}") 
+    print(f"Wrote {FUSEFILE1}")
 
 
 def main(verbose=False):
-    aliases, lmc, smc, tts = parse_inputs()
+    aliases, lmc, smc, tts, lowz = parse_inputs()
     fuse_alias1 = pd.read_json(FUSEFILE1, orient="split")
     fuse_alias2 = pd.read_json(FUSEFILE2, orient="split")
-    for targetlist in [smc, lmc, tts, fuse_alias1, fuse_alias2]:
+    for targetlist in [smc, lmc, tts, lowz, fuse_alias1, fuse_alias2]:
         aliases = add_aliases(targetlist, aliases, verbose)
     aliases = fix_pipes(aliases)
     aliases = aliases.apply(lambda x: x.astype(str).str.upper())
     aliases = aliases.applymap(lambda x: x.strip() if isinstance(x, str) else x)
     aliases.to_json("inputs/pd_all_aliases.json", orient="split")
-    aliases.to_csv("inputs/pd_all_aliases.csv") 
+    aliases.to_csv("inputs/pd_all_aliases.csv")
     print("Wrote inputs/pd_all_aliases.json and pd_all_aliases.csv")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", default=False,
                         action="store_true",
-                        help="If True, print what aliases were found or added") 
+                        help="If True, print what aliases were found or added")
     args = parser.parse_args()
     main(args.verbose)
