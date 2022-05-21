49a4476125293c4d0d4a9393b7f11cd6ec02ae14
Jo Taylor <jotaylor@stsci.edu>
2022-05-21 16:44:24 -0400

    Add new targets that have periods

--- utils/ullyses_config.py
+++ utils/ullyses_config.py
@@ -3,11 +3,17 @@ CAL_VER = 1.1
 
 # Some targets have periods in their name and these can break MAST ingest
 # Rename them to remove periods and strip any trailing numbers after periods
-RENAME = {"moa-j010321.3-720538": "moa-j010321-720538",
-          "sstc2dj160830.7-382827": "sstc2dj160830-382827",
-          "echa-j0844.2-7833": "echa-j0844-7833",
-          "sstc2dj160000.6-422158": "sstc2dj160000-422158",
+RENAME = {
           "echa-j0843.3-7915": "echa-j0843-7915",
+          "echa-j0844.2-7833": "echa-j0844-7833",
+          "moa-j010321.3-720538": "moa-j010321-720538",
           "ogle-j004942.75-731717.7": "ogle-j004942-731717",
-          "sstc2dj161344.1-373646": "sstc2dj161344-373646"}
+          "pgz2001-j161031.9-191305": "pgz2001-j161031-191305",
+          "pz99-j161019.1-250230": "pz99-j161019-250230",
+          "rxj0438.6+1546": "rxj0438+1546",
+          "sstc2dj160000.6-422158": "sstc2dj160000-422158",
+          "sstc2dj160830.7-382827": "sstc2dj160830-382827",
+          "sstc2dj161243.8-381503": "sstc2dj161243-381503"
+          "sstc2dj161344.1-373646": "sstc2dj161344-373646",
+          }
 
