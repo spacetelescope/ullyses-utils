486a02728fb0219125735f7e9ef09b0f5c05f6fb
Rachel Plesha <rplesha@stsci.edu>
2023-02-01 10:32:12 -0500

    Several changes for the website tables to work with the database files.
    - A new function to parse the two database files instead of the many target tables.
    - Updates to the 'ULL NAME' in the alias files to be consistent with the standard name in the database files.
    - Alex's draft change for the massive star database file, including updates to the lowZ photometry

--- utils/parse_csv.py
+++ utils/parse_csv.py
@@ -1,6 +1,8 @@
 from astropy.table import Table
 import pandas as pd
 import os
+import numpy as np
+
 dirname = os.path.dirname(__file__)
 
 name_csvs = {"lmc": "LMC_Preferred_Names.csv",
@@ -13,6 +15,12 @@ target_csvs = {"lmc":  ["LMC_sample_for_website_clean_newcoord_all_columns_order
                "ctts": ["classical-t-tauri-star-monitoring-targets.csv"],
                "tts" : ["Cha I_sample_for_website.csv", "CrA_sample_for_website.csv", "Lupus_sample_for_website.csv", "Ori OB1_sample_for_website.csv", "Sigma Ori_sample_for_website.csv", "eps Cha_sample_for_website.csv", "eta Cha_sample_for_website.csv", "Taurus_sample_for_website.csv", "TWA_sample_for_website.csv", "Lower Centaurus_sample_for_website.csv", "Upper Sco_sample_for_website.csv", "other_sample_for_website.csv"],
                "all": []}
+database_csvs = {"lmc":  ["all_massive_star_metadata.csv"],
+                 "smc" : ["all_massive_star_metadata.csv"],
+                 "lowz": ["all_massive_star_metadata.csv"],
+                 "ctts": ["full_sample_CTTS_for_DB.csv"],
+                 "tts" : ["full_sample_CTTS_for_DB.csv"],
+                 "all": []}
 
 def parse_name_csv(target_type, returndf=False):
     target_type = target_type.lower()
@@ -45,6 +53,32 @@ def parse_target_csv(target_type):
             #     remove_rows = df[(df['COS'] == 0) & (df['STIS'] == 0)]
             #     df = df.drop(df.index[remove_rows.index])
             #     print(f'Not including: {remove_rows}')
+            dfs.append(df)
+        except:
+            print("ERROR! Could not read file, skipping {}".format(csvfile))
+            csvs.pop(csvs.index(csvfile0))
+            continue
+
+    return csvs, dfs
+
+def parse_database_csv(target_type):
+
+    # using the dictionary defined above to map a galaxy/stellar region to the CSV file that contains info about it
+    #   in the database era there are only 2 csv files now; 1 for high mass and 1 for low mass
+    target_type = target_type.lower()
+    assert target_type in database_csvs, "Target type not recognized; acceptable values are 'lmc', 'smc', 'lowz', 'ctts', 'tts', 'all'"
+
+    # select the individual target file, or the combined target files
+    if target_type == "all":
+        csvs = list(np.unique(np.concatenate([tcsv for ttype, tcsv in database_csvs.items()])))
+    else:
+        csvs = database_csvs[target_type]
+
+    dfs = []
+    for csvfile0 in csvs:
+        csvfile = os.path.join(dirname, "data/target_metadata", csvfile0)
+        try:
+            df = pd.read_csv(csvfile)
             dfs.append(df)
         except:
             print("ERROR! Could not read file, skipping {}".format(csvfile))
