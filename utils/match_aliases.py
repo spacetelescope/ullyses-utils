cceaf39fc6b6d2fc992cd23231bd3e7e93fb4024
Jo Taylor <jotaylor@stsci.edu>
2023-07-20 11:55:59 -0400

    Add docstring, remove testing line

--- utils/match_aliases.py
+++ utils/match_aliases.py
@@ -6,6 +6,17 @@ RED = "\033[1;31m"
 RESET = "\033[0;0m"
 
 def match_aliases(targname, return_name="ULL_MAST_name"):
+    """
+    Given a target name, match it to any aliases in the ULLYSES alias file.
+
+    Args:
+        targname (str): Target name to match to.
+        return_name (str): Column in alias file to return as a matched name.
+            Default is ULL_MAST_name
+    Returns:
+        ull_targname (str): Matched alias.
+    """
+
     aliases_file = ullyses_utils.__path__[0] + '/data/target_metadata/pd_all_aliases.json'
     aliases = pd.read_json(aliases_file, orient="split")
     # In case we can't find a match
@@ -22,7 +33,3 @@ def match_aliases(targname, return_name="ULL_MAST_name"):
         print(f"{RED}WARNING: Could not match target name {ull_targname} to ULLYSES alias list{RESET}")
     return ull_targname
 
-
-if __name__ == "__main__":
-    alias = match_aliases("[AAOMEGA]_361")
-    print(alias)
