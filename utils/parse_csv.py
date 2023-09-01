65138b7ab10fc52e3579a24e1ee09df2db343357
Jo Taylor <jotaylor@stsci.edu>
2023-09-01 16:12:58 -0400

    Remove obsolete function since data files are no longer used

--- utils/parse_csv.py
+++ utils/parse_csv.py
@@ -9,12 +9,6 @@ name_csvs = {"lmc": "LMC_Preferred_Names.csv",
              "smc": "SMC_Preferred_Names.csv",
              "tts": "lowmass_preferred_names.csv",
              "lowz": "lowz_preferred_names.csv"}
-target_csvs = {"lmc":  ["LMC_sample_for_website_clean_newcoord_all_columns_order.csv"],
-               "smc" : ["SMC_sample_for_website_clean_newcoord_and_names_all_columns.csv"],
-               "lowz": ["low-metallicity-galaxy-targets.csv"],
-               "ctts": ["classical-t-tauri-star-monitoring-targets.csv"],
-               "tts" : ["Cha I_sample_for_website.csv", "CrA_sample_for_website.csv", "Lupus_sample_for_website.csv", "Ori OB1_sample_for_website.csv", "Sigma Ori_sample_for_website.csv", "eps Cha_sample_for_website.csv", "eta Cha_sample_for_website.csv", "Taurus_sample_for_website.csv", "TWA_sample_for_website.csv", "Lower Centaurus_sample_for_website.csv", "Upper Sco_sample_for_website.csv", "other_sample_for_website.csv"],
-               "all": []}
 database_csvs = {"lmc":  ["all_massive_star_metadata.csv"],
                  "smc" : ["all_massive_star_metadata.csv"],
                  "lowz": ["all_massive_star_metadata.csv", "lowz_db_metadata.csv"],
@@ -33,33 +27,6 @@ def parse_name_csv(target_type, returndf=False):
 
     return out
 
-def parse_target_csv(target_type):
-    target_type = target_type.lower()
-    assert target_type in target_csvs, "Target type not recognized; acceptable values are 'lmc', 'smc', 'lowz', 'ctts', 'tts', 'all'"
-    dfs = []
-    if target_type == "all":
-        csvs = []
-        for k in target_csvs:
-            csvs += target_csvs[k]
-    else:
-        csvs = target_csvs[target_type]
-
-    for csvfile0 in csvs:
-        csvfile = os.path.join(dirname, "data/target_metadata", csvfile0)
-        try:
-            df = pd.read_csv(csvfile)
-            # if 'metallicity' in csvfile:
-            #     # Removing WFC3 rows
-            #     remove_rows = df[(df['COS'] == 0) & (df['STIS'] == 0)]
-            #     df = df.drop(df.index[remove_rows.index])
-            #     print(f'Not including: {remove_rows}')
-            dfs.append(df)
-        except:
-            print("ERROR! Could not read file, skipping {}".format(csvfile))
-            csvs.pop(csvs.index(csvfile0))
-            continue
-
-    return csvs, dfs
 
 def parse_database_csv(target_type):
 
