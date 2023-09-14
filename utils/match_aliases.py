47727b7386e8da9dba3fc140912eefae50023c56
Rachel Plesha <rplesha@stsci.edu>
2023-09-14 10:28:42 -0400

    Updating documentation in match_aliases function

--- utils/match_aliases.py
+++ utils/match_aliases.py
@@ -2,7 +2,7 @@ import ullyses_utils
 import pandas as pd
 import re
 
-RED = "\033[1;31m" 
+RED = "\033[1;31m"
 RESET = "\033[0;0m"
 
 def match_aliases(targname, return_name="target_name_hlsp"):
@@ -12,16 +12,17 @@ def match_aliases(targname, return_name="target_name_hlsp"):
     Args:
         targname (str): Target name to match to.
         return_name (str): Column in alias file to return as a matched name.
-            Default is ULL_MAST_name
+            Default is target_name_hlsp.
+            Other options are target_name_ullyses & target_name_simbad.
     Returns:
-        ull_targname (str): Matched alias.
+        ull_targname (str): Matched alias to return_name column.
     """
 
     aliases_file = ullyses_utils.__path__[0] + '/data/target_metadata/ullyses_aliases.csv'
     aliases = pd.read_csv(aliases_file)
     # In case we can't find a match
     ull_targname = targname
-    
+
     targ_matched = False
     # The alias file is all uppercase
     targ_upper = targname.upper()
@@ -32,4 +33,3 @@ def match_aliases(targname, return_name="target_name_hlsp"):
     if targ_matched is False:
         print(f"{RED}WARNING: Could not match target name {ull_targname} to ULLYSES alias list{RESET}")
     return ull_targname
-
